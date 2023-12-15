7d6757f7615393c179e637cb4fe6a5e8c63ade2a
Ayemenre Oseghale <oseayemenre04@gmail.com>
2023-12-15 15:17:36 +0100

    another commiy

--- components/hero.tsx
+++ components/hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
   const { title } = useTitleChange();
   return (
     <section className='relative overflow-x-hidden h-[800px]'>
-      <div className='w-[1500px] bg-red-800 h-[800px] absolute -left-20'>
+      <div className='w-[1500px] bg-red-800 h-[800px] absolute -left-20 max-lg:w-[1000px] max-lg:-left-[100px] max-md:w-[600px] max-md:-left-[110px]'>
         <Image
           src='/island_2.jpg'
           width={3000}
@@ -19,7 +19,7 @@ export default function Hero() {
         />
       </div>
 
-      <div className='w-[1500px] h-[800px] rounded-b-[50%] absolute bg-[#0D233E] -left-20 opacity-[0.70]' />
+      <div className='w-[1500px] h-[800px] rounded-b-[50%] absolute bg-[#0D233E] -left-20 opacity-[0.70] max-lg:w-[1000px] max-lg:-left-[100px] max-md:w-[600px] max-md:-left-[110px]' />
 
       <div className='absolute w-full flex justify-center -left-[150px] items-center h-full'>
         <h1 className='text-[50px] font-[600] text-white'>
