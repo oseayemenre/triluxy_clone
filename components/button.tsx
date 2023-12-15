c6a975d5b3af614c60edb0653b43bd8f09f433d7
Ayemenre Oseghale <oseayemenre04@gmail.com>
2023-12-15 15:01:01 +0100

    another commit

--- components/button.tsx
+++ components/button.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { button_labels } from "@/utils/data";
+import { useState } from "react";
 
 type TButton = (typeof button_labels)[number];
 
@@ -11,9 +14,18 @@ export default function Button({
   hoverText,
   hoverColor,
 }: TButton) {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <button
-      className={`bg-[${bgColor}] border-[${borderColor}] border-[1px] text-[${textColor}] px-[15.7px] py-[7.5px] font-[600] rounded-[4px] focus:outline-none hover:text-[${hoverText}] hover:border-[${hoverBorder}] hover:bg-[${hoverColor}] duration-150 ease transition`}
+      style={{
+        backgroundColor: isHovered ? hoverColor : bgColor,
+        borderColor: isHovered ? hoverBorder : borderColor,
+        color: isHovered ? hoverText : textColor,
+      }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      className={`border-[1px]  px-[15.7px] py-[7.5px] font-[600] rounded-[4px] focus:outline-none duration-150 ease transition`}
     >
       {label}
     </button>
